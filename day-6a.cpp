4a810add20c0021c5130b12b5ce0a5b20cf2994e
Jack Parsons <jackpar1113@gmail.com>
2022-12-05 22:28:33 -0700

    complete day 6

---
+++ day-6a.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool unique(list<char> l) {
+    set<char> s;
+    for(auto val: l) {
+        s.insert(val);
+    }
+
+    return s.size() == 4;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string line;
+    getline(cin, line);
+
+    list<char> l;
+    int left;
+    for(left = 0; left < 4; left++) {
+        l.push_back(line[left]);
+    }
+
+    if(unique(l)) {
+        cout << "4" << endl;
+        return 0;
+    }
+
+    for(; left < line.size(); left++) {
+        l.pop_front();
+        l.push_back(line[left]);
+        if(unique(l)) {
+            cout << left+1 << endl;
+            return 0;
+        }
+    }
+}
